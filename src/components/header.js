abfc01ac9bfbd8fcd82c3e91ac3566093e7bc5bf
antonstrand <jag@antonstrand.se>
2020-04-12 14:03:06 +0200

    Move styling to layout.css

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,16 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
+import Logo from "./logo"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+const Header = () => (
+  <header>
+    <Link to='/'>
+      <Logo alt='An illustration of me by me' className='logo' />
+    </Link>
+    <span>
+      <Link to='/about'>About</Link>
+    </span>
   </header>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
